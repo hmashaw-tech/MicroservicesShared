bc82bc28a3094e7dfdc3429a58faf92cff0a9f1d
hmashaw <hmashaw@gmail.com>
2022-02-12 17:04:26 -0500

    QueryPost Update

--- Sources/MicroservicesShared/Models_v1/QueryPost.swift
+++ Sources/MicroservicesShared/Models_v1/QueryPost.swift
@@ -9,11 +9,11 @@ import Vapor
 
 public struct QueryPost: Content {
     
-    public var id: String?
+    public var id: String
     public var title: String
-    public var comments: [Comment?]?
+    public var comments: [Comment?]
     
-    public init(id: String?, title: String, comments: [Comment?]?) {
+    public init(id: String, title: String, comments: [Comment?]) {
         self.id = id
         self.title = title
         self.comments = comments
