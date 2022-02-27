04cd1a47441cd086b1ec944517bd7a30285c8acd
hmashaw <hmashaw@gmail.com>
2022-02-26 23:50:38 -0500

    Endpoint.printMe

--- Sources/MicroservicesShared/EnumEndpoint.swift
+++ Sources/MicroservicesShared/EnumEndpoint.swift
@@ -12,6 +12,14 @@ public enum EndPoint {
     case k8s
     case env
     
+    public var printMe: String {
+        if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
+            return "APP_ENV=\(value)"
+        } else {
+            return "APP_ENV matching .k8s"
+        }
+    }
+    
     public var postsService: String {
         switch self {
         case .local:
@@ -20,7 +28,6 @@ public enum EndPoint {
             return "http://posts-srv:4000/events"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                print("APP_ENV=\(value)")
                 return "\(value):4000/events"
             } else {
                 return "http://posts-srv:4000/events"
@@ -36,7 +43,6 @@ public enum EndPoint {
             return "http://comments-srv:4001/events"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                print("APP_ENV=\(value)")
                 return "\(value):4001/events"
             } else {
                 return "http://comments-srv:4001/events"
@@ -52,7 +58,6 @@ public enum EndPoint {
             return "http://query-srv:4002/events"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                print("APP_ENV=\(value)")
                 return "\(value):4002/events"
             } else {
                 return "http://query-srv:4002/events"
@@ -68,7 +73,6 @@ public enum EndPoint {
             return "http://moderation-srv:4003/events"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                print("APP_ENV=\(value)")
                 return "\(value):4003/events"
             } else {
                 return "http://moderation-srv:4003/events"
@@ -84,7 +88,6 @@ public enum EndPoint {
             return "http://event-bus-srv:4005/events"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                print("APP_ENV=\(value)")
                 return "\(value):4005/events"
             } else {
                 return "http://event-bus-srv:4005/events"
