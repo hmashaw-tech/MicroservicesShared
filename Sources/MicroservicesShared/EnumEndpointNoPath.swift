82e219dc2ec940b67db13a042a623eecb753fe1e
hmashaw <hmashaw@gmail.com>
2022-02-27 17:31:38 -0500

    enum EndpointNoPath

--- Sources/MicroservicesShared/EnumEndpointNoPath.swift
+++ Sources/MicroservicesShared/EnumEndpointNoPath.swift
@@ -1,13 +1,13 @@
 //
-//  EndPoint.swift
+//  EndpointNoPath.swift
 //  
 //
-//  Created by Mark Shaw on 2/25/22.
+//  Created by Mark Shaw on 2/27/22.
 //
 
 import Vapor
 
-public enum EndPoint {
+public enum EndpointNoPath {
     case local
     case k8s
     case env
@@ -23,14 +23,14 @@ public enum EndPoint {
     public var postsService: String {
         switch self {
         case .local:
-            return "http://localhost:4000/events"
+            return "http://localhost:4000"
         case .k8s:
-            return "http://posts-srv:4000/events"
+            return "http://posts-srv:4000"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                return "http://\(value):4000/events"
+                return "http://\(value):4000"
             } else {
-                return "http://posts-srv:4000/events"
+                return "http://posts-srv:4000"
             }
         }
     }
@@ -38,14 +38,14 @@ public enum EndPoint {
     public var commentsService: String {
         switch self {
         case .local:
-            return "http://localhost:4001/events"
+            return "http://localhost:4001"
         case .k8s:
-            return "http://comments-srv:4001/events"
+            return "http://comments-srv:4001"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                return "http://\(value):4001/events"
+                return "http://\(value):4001"
             } else {
-                return "http://comments-srv:4001/events"
+                return "http://comments-srv:4001"
             }
         }
     }
@@ -53,14 +53,14 @@ public enum EndPoint {
     public var queryService: String {
         switch self {
         case .local:
-            return "http://localhost:4002/events"
+            return "http://localhost:4002"
         case .k8s:
-            return "http://query-srv:4002/events"
+            return "http://query-srv:4002"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                return "http://\(value):4002/events"
+                return "http://\(value):4002"
             } else {
-                return "http://query-srv:4002/events"
+                return "http://query-srv:4002"
             }
         }
     }
@@ -68,14 +68,14 @@ public enum EndPoint {
     public var moderationService: String {
         switch self {
         case .local:
-            return "http://localhost:4003/events"
+            return "http://localhost:4003"
         case .k8s:
-            return "http://moderation-srv:4003/events"
+            return "http://moderation-srv:4003"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                return "http://\(value):4003/events"
+                return "http://\(value):4003"
             } else {
-                return "http://moderation-srv:4003/events"
+                return "http://moderation-srv:4003"
             }
         }
     }
@@ -83,14 +83,14 @@ public enum EndPoint {
     public var eventsbusService: String {
         switch self {
         case .local:
-            return "http://localhost:4005/events"
+            return "http://localhost:4005"
         case .k8s:
-            return "http://event-bus-srv:4005/events"
+            return "http://event-bus-srv:4005"
         case .env:
             if let value = ProcessInfo.processInfo.environment["APP_ENV"] {
-                return "http://\(value):4005/events"
+                return "http://\(value):4005"
             } else {
-                return "http://event-bus-srv:4005/events"
+                return "http://event-bus-srv:4005"
             }
         }
     }
